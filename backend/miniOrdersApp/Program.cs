96faffccbc945012adae9a073ed2b1302967a1d3
willianwt2000 <willianmetelien@gmail.com>
2025-10-03 10:21:48 -0400

    feat(cors): add CORS policy to allow all origins

--- backend/miniOrdersApp/Program.cs
+++ backend/miniOrdersApp/Program.cs
@@ -8,7 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
---
+++ frontend/mini-order-app/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+ const dataURl = async() => {
+  const response = await fetch("http://localhost:5172/orders")
+  const data = await response.json()
+  console.log(data)
+
+ }
+ 
+
+ onMounted(() => {
+  dataURl()
+ }) 
+</script>
